3754ceb6084433975ea21bb5f5dcf6901d752562
joedegaetano <116955260+joedegaetano@users.noreply.github.com>
2024-04-07 19:56:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import pandas as pd
+from helper_functions import shell_sort
+
+# Turn off data frame console display limitations
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
 
 # Read JSON file into DataFrame
 df = pd.read_json("yelp_academic_dataset_business.json", lines=True)
 
-# Count the number of records
-num_records = len(df)
+# Filter only restaurant records
+filtered_df = df[df['categories'].str.contains('Restaurants', case=False, na=False)]
 
-# Print the number of records
-print("Number of records:", num_records)
+# Drop unnecessary columns
+reduced_df = filtered_df.drop(columns=['business_id', 'hours', 'is_open', 'latitude', 'longitude', 'attributes'])
 
-b_pandas = []
-r_dtypes = {"name": str}
+# Convert DataFrame to a list of dictionaries
+business_list = reduced_df.to_dict(orient='records')
 
-with open("yelp_academic_dataset_business.json", "r") as f:
-    reader = pd.read_json(f, orient="records", lines=True, dtype=r_dtypes, chunksize=1000)
 
-    for chunk in reader:
-        filtered_chunk = chunk[chunk['categories'].str.contains('Restaurants', case=False, na=False)]
-        reduced_chunk = filtered_chunk.drop(columns=['business_id', 'hours', 'is_open'])
-        b_pandas.append(reduced_chunk)
+# Sort the list of dictionaries using Shell Sort
+shell_sort(business_list)
 
-b_pandas = pd.concat(b_pandas, ignore_index=True)
-print(b_pandas)
+# Convert the sorted list back to a DataFrame
+sorted_df = pd.DataFrame(business_list)
+sorted_df = sorted_df[sorted_df['stars'] > 4.0]
+print(sorted_df.head(1000))
